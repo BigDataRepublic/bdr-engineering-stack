1d6354ef456aff4c23d079a6dcd1a728e712a930
Stefan van Wouw <stefanvanwouw@gmail.com>
2016-12-12 21:54:29 +0100

    Release 0.1.0 (#2)
    * Initial code, Kafka Connect still needs a fix and cleaning dirs afterwards is not yet correctly implemented
    
    * Moved to subdir
    
    * Working version including assembly fat jar.
    
    * Prepare for first release
    
    * Travis intregration (#1)
    
    Added travis build integration for running sbt builds and tests.

---
+++ embedded-streaming-services/app/src/main/scala/nl/bigdatarepublic/streaming/embedded/adapter/kafka/EmbeddedKafka.scala
@@ -0,0 +1,55 @@
+package nl.bigdatarepublic.streaming.embedded.adapter.kafka
+
+
+import java.io.File
+import java.util.Properties
+
+import com.typesafe.scalalogging.LazyLogging
+import kafka.server.{KafkaConfig, KafkaServerStartable}
+import nl.bigdatarepublic.streaming.embedded.entity.EmbeddedService
+import org.apache.commons.io.FileUtils
+
+import scala.collection.JavaConverters._
+import scala.util.{Failure, Success, Try}
+
+
+class EmbeddedKafka(props: Map[String, String], clearState: Boolean) extends LazyLogging with EmbeddedService {
+
+  val kafka: KafkaServerStartable = new KafkaServerStartable(KafkaConfig(props.asJava))
+
+  def start(): Unit = {
+    // Clear out the existing kafka dir upon startup.
+    if (clearState) {
+      logger.info("Cleaning Kafka data dir before start...")
+      kafka.serverConfig.logDirs.foreach { x =>
+        Try(FileUtils.cleanDirectory(new File(x))) match {
+          case Success(prop) => logger.info("Successfully cleaned Kafka data dir...")
+          case Failure(e) => logger.error("Failed to clean Kafka data dir", e)
+        }
+      }
+    }
+    logger.info("Starting embedded Kafka...")
+    kafka.startup()
+    logger.info("Successfully started embedded Kafka")
+
+  }
+
+  def stop(): Unit = {
+    logger.info("Stopping embedded Kafka...")
+    kafka.shutdown()
+    logger.info("Successfully stopped embedded Kafka")
+
+  }
+
+}
+
+
+
+object EmbeddedKafka {
+  def apply(props: Map[String, String], clearState: Boolean): EmbeddedKafka = new EmbeddedKafka(props, clearState)
+  def apply(props: Map[String, String]): EmbeddedKafka = new EmbeddedKafka(props, false)
+
+  // Java compatibility
+  def apply(props: Properties, clearState: Boolean): EmbeddedKafka = new EmbeddedKafka(props.asScala.toMap, clearState)
+  def apply(props: Properties): EmbeddedKafka = new EmbeddedKafka(props.asScala.toMap, false)
+}
