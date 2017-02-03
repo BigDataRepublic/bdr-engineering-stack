fa841b5bea76441867bf7cfc1fed980cb2bab4fa
Sander Ardinois <sander.ardinois@gmail.com>
2017-02-03 15:14:59 +0100

    include package data

--- data-science-production-container/ds_prod_api/setup.py
+++ data-science-production-container/ds_prod_api/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='ds_prod_api',
 
-    packages=['ds_prod_api', 'ds_prod_api.abstracts', 'ds_prod_api.apis'],
-    version='0.1',
-
-    entry_points={
-        'console_scripts': [
-        'ds_prod_api = ds_prod_api.command_line:main']
-    })
+packages=['ds_prod_api', 'ds_prod_api.abstracts', 'ds_prod_api.apis'],
+include_package_data=True,
+version='0.1',
+entry_points={
+    'console_scripts': [
+    'ds_prod_api = ds_prod_api.command_line:main']
+})
